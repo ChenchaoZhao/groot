e63e5a68d53831af6d929446f5ac06c38ed90fa7
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-07-22 22:51:52 -0400

    init

---
+++ groot/__init__.py
@@ -0,0 +1,8 @@
+r"""
+The package helps manage hierarchical concept trees.
+
+Each tree is serialized as a yaml file which can be loaded to construct the python
+class.
+"""
+
+__version__ = "0.0.0"
