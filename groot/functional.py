cc5ef51fd64c64b175c3fd435d38637898e6a0eb
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-07-24 15:19:05 -0400

    add level func

--- groot/functional.py
+++ groot/functional.py
@@ -111,3 +111,51 @@ def all_leaf_nodes(root: str, nodes: Dict[str, Node]) -> Set[str]:
         leaf_nodes.update(all_leaf_nodes(c, nodes))
 
     return leaf_nodes
+
+
+def index_nodes_by_level(
+    nodes: Dict[str, Node]
+) -> Tuple[Dict[str, int], List[Set[str]]]:
+    """
+    Assign levels to given nodes.
+
+    Parameters
+    ----------
+    nodes : Dict[str, Node]
+        Lookup table of nodes.
+
+    Returns
+    -------
+    Tuple[Dict[str, int], List[Set[str]]]
+        Level lookup table, and levels of nodes.
+    """
+
+    node_level = {name: None for name in nodes}
+
+    def _get_level(node: Node) -> int:
+
+        if node_level[node.name] is not None:
+            return node_level[node.name]
+
+        if node.is_root:
+            node_level[node.name] = 0
+            return 0
+
+        node_level[node.name] = _get_level(nodes[node.parent]) + 1
+
+        return node_level[node.name]
+
+    for node in nodes.values():
+        _ = _get_level(node)
+
+    levels = {}
+
+    for k, v in node_level.items():
+        if v in levels:
+            levels[v].add(k)
+        else:
+            levels[v] = {k}
+
+    levels = [levels[l] for l in range(len(levels))]
+
+    return node_level, levels
