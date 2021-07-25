bb50ccdc1ef83ff0e7ebdd70c07c79c7cf82a081
ChenchaoZhao <chenchao.zhao@gmail.com>
2021-07-25 00:23:22 -0400

    update doc str

--- groot/__init__.py
+++ groot/__init__.py
@@ -1,8 +1,16 @@
-r"""
+"""
 The package helps manage hierarchical concept trees.
 
-Each tree is serialized as a yaml file which can be loaded to construct the python
-class.
+Each tree can be serialized as a yaml file and each file can be loaded to
+construct the python class.
+
+Goals
+-----
+* Save the trees in most compact format
+* Be able to load the trees from serialized files
+* List all nodes
+* List nodes on a given depth
+* List all atomic nodes
 """
 
 __version__ = "0.1.0"
